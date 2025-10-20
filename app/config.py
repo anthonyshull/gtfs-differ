85dd6a84750b66191fa89eed2036ccf03c3d47d0
anthonyshull <shull.anthony@gmail.com>
2025-10-20 13:46:54 -0500

    try to save to workspace

--- app/config.py
+++ app/config.py
@@ -11,11 +11,12 @@ from app.helpers import parse_last_modified
 
 script_path = Path(__file__).resolve()
 project_root = script_path.parent.parent
+github_workspace = environ.get("GITHUB_WORKSPACE")
 
-WORKSPACE = Path(environ.get("GITHUB_WORKSPACE", project_root))
+WORKSPACE = Path(github_workspace) if github_workspace else project_root
 
 GTFS_URL = "https://cdn.mbta.com/MBTA_GTFS.zip"
-GTFS_DIR_PATH = project_root / Path("data/gtfs")
+GTFS_DIR_PATH = WORKSPACE / Path("data/gtfs")
 ZIP_GTFS_PATH = GTFS_DIR_PATH / Path(urlparse(GTFS_URL).path).name
 NEW_GTFS_PATH = GTFS_DIR_PATH / Path("new")
 OLD_GTFS_PATH = GTFS_DIR_PATH / Path("old")
