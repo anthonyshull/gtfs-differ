4cb2c767a77130afd38d53f3de116bd845706ba8
anthonyshull <shull.anthony@gmail.com>
2025-10-20 13:24:14 -0500

    different parser

--- app/config.py
+++ app/config.py
@@ -6,6 +6,8 @@ from datetime import datetime
 from pathlib import Path
 from urllib.parse import urlparse
 
+from helpers import parse_last_modified
+
 script_path = Path(__file__).resolve()
 project_root = script_path.parent.parent
 
@@ -16,4 +18,4 @@ NEW_GTFS_PATH = GTFS_DIR_PATH / Path("new")
 OLD_GTFS_PATH = GTFS_DIR_PATH / Path("old")
 
 LAST_MODIFIED = open("LAST_MODIFIED", encoding="utf-8").read().strip()
-LAST_MODIFIED_DATETIME = datetime.strptime(LAST_MODIFIED, "%a, %d %b %Y %H:%M:%S %Z")
+LAST_MODIFIED_DATETIME = parse_last_modified(LAST_MODIFIED)
