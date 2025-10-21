4bc3f5323279e5a390931970320ded69cb7e3856
anthonyshull <shull.anthony@gmail.com>
2025-10-21 15:55:21 -0500

    zip even if no files

--- app/steps/diff_gtfs.py
+++ app/steps/diff_gtfs.py
@@ -12,6 +12,7 @@ class DiffGTFS(Step):
     def process(self) -> Self:
         """Processes the step."""
         DIFF_GTFS_PATH.mkdir(parents=True, exist_ok=True)
+        DIFF_GTFS_PATH / Path(".gitkeep").touch()
 
         for item in DIFF_GTFS_PATH.iterdir():
             if item.is_file():
