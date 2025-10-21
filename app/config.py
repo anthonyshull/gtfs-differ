7fe0653983a4ec5ffcbffbf2336954aabbda252c
anthonyshull <shull.anthony@gmail.com>
2025-10-21 12:56:45 -0500

    set var

--- app/config.py
+++ app/config.py
@@ -13,6 +13,8 @@ _github_workspace = environ.get("GITHUB_WORKSPACE")
 
 WORKSPACE = Path(_github_workspace) if _github_workspace else _project_root
 
+GTFS_URL = environ.get("GTFS_URL")
+
 GTFS_DIR_PATH = WORKSPACE / Path("data/gtfs")
 DIFF_GTFS_PATH = GTFS_DIR_PATH / Path("diff")
 NEW_GTFS_PATH = GTFS_DIR_PATH / Path("new")
@@ -21,5 +23,3 @@ ZIP_GTFS_PATH = GTFS_DIR_PATH / Path(urlparse(GTFS_URL).path).name
 
 LAST_MODIFIED = open("LAST_MODIFIED", encoding="utf-8").read().strip()
 LAST_MODIFIED_DATETIME = parser.parse(LAST_MODIFIED)
-
-GTFS_URL = environ.get("GTFS_URL")
